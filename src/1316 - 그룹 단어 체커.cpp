73aecd0635bc781c53d64029a3309c7ff09cc527
Si-Yual Park <siyual.bak@gmail.com>
2019-04-02 14:10:21 +0900

    .

--- src/1316 - 그룹 단어 체커.cpp
+++ src/1316 - 그룹 단어 체커.cpp
@@ -3,34 +3,41 @@
 #include <string>
 #include <array>
 
+bool isGroupWord(const std::string& word);
+
 int main() {
     int n;
     std::cin >> n;
 
     int num_of_group_word {0};
-    std::array<bool, 'z' - 'a' + 1> is_before {};
     for (int i = 0; i < n; ++i) {
-        std::fill(is_before.begin(), is_before.end(), false);
-        
         std::string word {};
         std::cin >> word;
 
-        signed char before_ch = -1;
-        int j;
-        for (j = 0; j < word.length(); ++j) {
-            if (word[j] != before_ch)
-                if (is_before[word[j] - 'a'])
-                    break;
-                else
-                    is_before[word[j] - 'a'] = true;
-            before_ch = word[j];
-        }
-        
-        if (j == word.length())
+        if (isGroupWord(word))
             num_of_group_word++;
     }
 
     std::cout << num_of_group_word << std::endl;
 
     return 0;
+}
+
+bool isGroupWord(const std::string& word) {
+    std::array<bool, 'z' - 'a' + 1> is_before {};
+
+    signed char before_ch = -1;
+    int j;
+    for (j = 0; j < word.length(); ++j) {
+        if (word[j] != before_ch)
+            if (is_before[word[j] - 'a'])
+                break;
+            else
+                is_before[word[j] - 'a'] = true;
+        before_ch = word[j];
+    }
+
+    if (j == word.length())
+        return true;
+    return false;
 }
